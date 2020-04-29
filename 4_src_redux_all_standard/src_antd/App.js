37b7fea7f82a04a85baa5300715055f575ff9593
zhaoxuan <768556681@qq,com>
2020-04-29 21:27:23 +0800

    2

--- 4_src_redux_all_standard/src_antd/App.js
+++ 4_src_redux_all_standard/src_antd/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import Count from './containers/Count'
+import {Button} from 'antd'
 
 export default class App extends Component {
 	render() {
 		return (
-			<Count/>
+			<div>
+				App....。
+				<Button type="primary">点我</Button>
+			</div>
 		)
 	}
 }
